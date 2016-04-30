1990091f1b711b8b998b8a3f9193627e5765c3df
bonnou <earthly.desire@gmail.com>
2016-04-30 10:47:15 +0900

    heroku test

--- src/main/java/net/in/ahr/dbmsOnline/sample01/HelloApplication.java
+++ src/main/java/net/in/ahr/dbmsOnline/sample01/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication {
 
 	@RequestMapping("/")
 	String home() {
-		return "Hello World!!!!!";
+		return "Hello World! heroku";
 	}
 
 	public static void main(String[] args) {
